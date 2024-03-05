c9e37b2ffe838f82823af162ea775a34936c82d6
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-05 16:55:48 +0530

    Add files via upload

---
+++ print 2d array row wise wave form/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void waveRow(int[][] A){
+        int N= A.length;
+        int M= A[0].length;
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){System.out.print("("+i+")");System.out.print("("+j+")");
+                System.out.print(" "+ A[i][j] +" ");
+            }System.out.println();
+        }
+
+        System.out.println();System.out.println();
+
+        for (int i=0;i<N;i++){
+            if (i%2==0){
+            for (int j=0;j<M;j++){System.out.print("("+i+")");System.out.print("("+j+")");
+                System.out.print(" "+ A[i][j] +" ");
+            }}
+            else if(i%2!=0){
+                for (int j=M-1;j>=0;j--){System.out.print("("+i+")");System.out.print("("+j+")");
+                    System.out.print(" "+ A[i][j] +" ");
+                }
+            }
+        }
+
+    }
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner (System.in);
+        int N= sc.nextInt();
+        int M= sc.nextInt();
+
+        int[][] A= new int[N][M];
+        for (int i=0; i<N; i++){
+            for (int j=0; j<M; j++){
+                A[i][j] = sc.nextInt();
+            }
+        }
+        waveRow(A);
+    }
+}
