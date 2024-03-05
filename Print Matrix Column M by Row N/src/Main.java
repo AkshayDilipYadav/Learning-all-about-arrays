80afc0e204d508dc2b57caab3ed0dcab8300efda
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-05 15:37:40 +0530

    Add files via upload

---
+++ Print Matrix Column M by Row N/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void printColumnByRow (int [][] A){
+        int N = A.length;
+        int M = A[0].length;
+
+        for (int i = 0; i <N; i++){
+            for (int j=0; j <M; j++){System.out.print("("+i+")");System.out.print("("+j+")");
+                System.out.print(" "+ A[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(); System.out.println();
+        for (int j=0; j <M; j++){
+            for (int i = 0; i <N; i++){System.out.print("("+i+")");System.out.print("("+j+")");
+                System.out.print(" "+ A[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        int M= sc.nextInt();
+        int [][] A = new int [N][M];
+
+        for (int i = 0; i <N; i++){
+            for (int j=0; j <M; j++){
+                A[i][j]= sc.nextInt();
+            }
+        }
+
+        printColumnByRow(A);
+    }
+}
