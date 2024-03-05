6ad93ab62aee1abe9262638b13ef3069600e9040
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-03-05 17:48:15 +0530

    Add files via upload

---
+++ Print Min of each row/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void maxRow(int [][] A){
+        int N = A.length;
+        int M = A[0].length;
+
+        int [] ans = new int [N];
+
+        for (int i =0; i<N; i++){   int min= Integer.MAX_VALUE;
+            for (int j = 0; j<M; j++){
+                if (A[i][j]<min){min = A[i][j];}
+            }ans[i] = min;
+        }
+
+        for (int i =0; i<N; i++){
+            System.out.print(ans[i]+ " ");
+        }
+
+    }
+    public static void main (String[] args){
+        Scanner sc= new Scanner(System.in);
+        int N= sc.nextInt();
+        int M= sc.nextInt();
+
+        int [][] A= new int [N][M];
+        for (int i =0; i<N; i++){
+            for (int j =0; j<M; j++){
+                A[i][j]= sc.nextInt();
+            }}
+        maxRow(A);
+    }
+}
